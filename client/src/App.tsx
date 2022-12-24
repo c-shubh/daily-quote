d9b1d4b9ac226a86ea3fcb399d4aaac9c5c7d5e1
Shubh Chudasama <shubh.cs@proton.me>
2022-12-24 12:19:38 +0530

    Add date picker

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,7 @@
 import dayjs from "dayjs";
+import { DayPicker, SelectSingleEventHandler } from "react-day-picker";
+import "react-day-picker/dist/style.css";
+
 import { useEffect, useState } from "react";
 import { FiChevronLeft, FiChevronRight } from "react-icons/fi";
 
@@ -7,6 +10,36 @@ function ArrowButton(props: { direction: "left" | "right"; onClick?: any }) {
     <button type="button" className="block border rounded px-2" onClick={props.onClick}>
       {props.direction == "left" ? <FiChevronLeft /> : <FiChevronRight />}
     </button>
+  );
+}
+
+function DayPickerDialog(props: {
+  selected: Date;
+  onSelect: SelectSingleEventHandler;
+  open: boolean;
+  setOpen: React.Dispatch<React.SetStateAction<boolean>>;
+}) {
+  const mapping = { true: "", false: "hidden" };
+  return (
+    <div
+      className={`absolute inset-0 bg-[hsl(0deg_0%_0%_/_80%)] flex justify-center grid grid-rows-4 ${
+        mapping[props.open + ""]
+      }`}
+      onClick={() => props.setOpen(!props.open)}
+    >
+      <div onClick={(e) => e.stopPropagation()} className="row-start-2">
+        <DayPicker
+          mode="single"
+          captionLayout="dropdown"
+          selected={props.selected}
+          fromYear={props.selected.getFullYear()}
+          toYear={props.selected.getFullYear()}
+          onSelect={props.onSelect}
+          className="bg-white border rounded p-4 shadow-md"
+          onDayClick={() => props.setOpen(false)}
+        />
+      </div>
+    </div>
   );
 }
 
@@ -29,6 +62,8 @@ export default function App() {
     }
   );
 
+  const [dialogOpen, setDialogOpen] = useState(false);
+
   useEffect(() => {
     async function asyncInsideUseEffect() {
       setQuote(await fetchQuote(date));
@@ -45,10 +80,24 @@ export default function App() {
 
   return (
     <div className="prose mx-6 py-6 flex flex-col space-y-4 h-full">
+      <DayPickerDialog
+        selected={date.toDate()}
+        onSelect={(d: Date) => setDate(dayjs(d))}
+        open={dialogOpen}
+        setOpen={setDialogOpen}
+      />
       <h2 className="text-center border rounded-lg p-2 mb-0">Daily Quote</h2>
       <div className="border rounded-lg flex justify-between p-2">
         <ArrowButton direction="left" onClick={() => previousDate()} />
-        <span className="font-bold">{date.format("D MMMM")}</span>
+        <button
+          type="button"
+          className="font-bold"
+          onClick={() => {
+            setDialogOpen(!dialogOpen);
+          }}
+        >
+          {date.format("D MMMM")}
+        </button>
         <ArrowButton direction="right" onClick={() => nextDate()} />
       </div>
       <div className="border rounded-lg p-4 flex-grow-1 h-fit overflow-auto">
