036247c7e4f6c57845e399243b1009ccfecadad7
Shubh Chudasama <shubh.cs@proton.me>
2022-12-30 20:21:16 +0530

    Convert to NextJS: add, modify files
    Rename env files
    
    Fix http-equiv to httpEquiv

---
+++ client/pages/_app.tsx
@@ -0,0 +1,17 @@
+import type { AppProps } from "next/app";
+import Head from "next/head";
+import "../styles/globals.css";
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Head>
+        <meta charSet="UTF-8" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
+}
--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,8 +1,8 @@
 import dayjs from "dayjs";
+import Head from "next/head";
+import { useEffect, useState } from "react";
 import { DayPicker, SelectSingleEventHandler } from "react-day-picker";
 import "react-day-picker/dist/style.css";
-
-import { useEffect, useState } from "react";
 import { FiChevronLeft, FiChevronRight } from "react-icons/fi";
 
 function ArrowButton(props: { direction: "left" | "right"; onClick?: any }) {
@@ -45,12 +45,12 @@ function DayPickerDialog(props: {
 
 /* Fetch quote for given `date` from db */
 async function fetchQuote(date: dayjs.Dayjs) {
-  const response = await fetch(`${import.meta.env.VITE_API_URL}/${date.format("MMMM")}/${date.format("D")}`);
+  const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/${date.format("MMMM")}/${date.format("D")}`);
   const quote_resp = await response.json();
   return quote_resp;
 }
 
-export default function App() {
+export default function Home() {
   /* Store date of currently visible quote */
   const [date, setDate] = useState(dayjs());
 
@@ -79,31 +79,36 @@ export default function App() {
   }
 
   return (
-    <div className="prose mx-6 py-6 flex flex-col space-y-4 h-full">
+    <>
+      <Head>
+        <title>Daily Quote</title>
+      </Head>
       <DayPickerDialog
         selected={date.toDate()}
         onSelect={(d) => setDate(dayjs(d))}
         open={dialogOpen}
         setOpen={setDialogOpen}
       />
-      <h2 className="text-center border rounded-lg p-2 mb-0">Daily Quote</h2>
-      <div className="border rounded-lg flex justify-between p-2">
-        <ArrowButton direction="left" onClick={() => previousDate()} />
-        <button
-          type="button"
-          className="font-bold"
-          onClick={() => {
-            setDialogOpen(!dialogOpen);
-          }}
-        >
-          {date.format("D MMMM")}
-        </button>
-        <ArrowButton direction="right" onClick={() => nextDate()} />
+      <div className="prose mx-6 py-6 flex flex-col space-y-4 h-full">
+        <h2 className="text-center border rounded-lg p-2 m-0">Daily Quote</h2>
+        <div className="border rounded-lg flex justify-between p-2">
+          <ArrowButton direction="left" onClick={() => previousDate()} />
+          <button
+            type="button"
+            className="font-bold"
+            onClick={() => {
+              setDialogOpen(!dialogOpen);
+            }}
+          >
+            {date.format("D MMMM")}
+          </button>
+          <ArrowButton direction="right" onClick={() => nextDate()} />
+        </div>
+        <div className="border rounded-lg p-4 flex-grow-1 h-fit overflow-auto">
+          <h4 className="mt-0 text-center">{!quote ? "" : quote.topic}</h4>
+          <p className="mb-0">{!quote ? "" : quote.text}</p>
+        </div>
       </div>
-      <div className="border rounded-lg p-4 flex-grow-1 h-fit overflow-auto">
-        <h4 className="mt-0 text-center">{!quote ? "" : quote.topic}</h4>
-        <p className="mb-0">{!quote ? "" : quote.text}</p>
-      </div>
-    </div>
+    </>
   );
 }
