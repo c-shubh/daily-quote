29829e6ece557435ae2b50513abb3d5503797a9f
Shubh Chudasama <shubh.cs@proton.me>
2023-01-02 19:45:35 +0530

    Fix nextjs base path

--- client/next.config.js
+++ client/next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  basePath: "/daily-quote",
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
