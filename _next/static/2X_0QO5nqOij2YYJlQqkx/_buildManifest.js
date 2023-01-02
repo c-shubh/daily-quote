a36dccfdd306c90eb5d549b52e898907d7aab290
Shubh Chudasama <shubh.cs@proton.me>
2023-01-02 19:32:02 +0530

    from 02bb9a77743cacda38d65fb955d50b368f88aa4f

---
+++ _next/static/2X_0QO5nqOij2YYJlQqkx/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/602-cc4065bb683131e3.js","static/css/b270b2686d361e13.css","static/chunks/pages/index-7fb47483743aecaf.js"],"/_error":["static/chunks/pages/_error-82a61e6508dae5a8.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
